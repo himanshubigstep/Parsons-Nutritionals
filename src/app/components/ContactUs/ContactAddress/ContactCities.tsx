4f4776ce70c8eb705e86cd5565c4356ad2c497f9
Himanshu Singh <himanshu.singh@bigsteptech.com>
2024-10-09 18:36:50 +0530

    final-issues

--- src/app/components/ContactUs/ContactAddress/ContactCities.tsx
+++ src/app/components/ContactUs/ContactAddress/ContactCities.tsx
@@ -3,12 +3,12 @@
 import React, { useState } from 'react';
 import MapCanvas from './Mapcanvas';
 
-const ContactCities = ({ mapLocations }: { mapLocations: any }) => {
+const ContactCities = ({ mapLocations, applyFilter }: { mapLocations: any, applyFilter?: boolean }) => {
   const [hoveredCity, setHoveredCity] = useState<string | null>(null);
 
   return (
     <div className="relative w-full h-full">
-      <MapCanvas locations={mapLocations} />
+      <MapCanvas locations={mapLocations} applyFilter={applyFilter} />
       {hoveredCity && (
         <div className="absolute top-0 right-0 bg-white dark:bg-black dark:border-[1px] dark:border-gray-700 p-2 rounded shadow">
           <p>{hoveredCity}</p>
