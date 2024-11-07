97a4da6b63ae43ff75e68854979e2c985bf72ebd
yashu <yashu.s@bigsteptech.com>
2024-11-07 19:24:00 +0530

    map canvas css revert

--- src/app/components/ContactUs/ContactAddress/ContactCities.tsx
+++ src/app/components/ContactUs/ContactAddress/ContactCities.tsx
@@ -10,7 +10,7 @@ const ContactCities = ({ mapLocations, applyFilter }: { mapLocations: any, apply
     <div className="w-full h-full">
       <MapCanvas locations={mapLocations} applyFilter={applyFilter} />
       {hoveredCity && (
-        <div className="absolute top-200 right-0 bg-white dark:bg-black dark:border-[1px] dark:border-gray-700 p-2 rounded shadow">
+        <div className="absolute top-0 right-0 bg-white dark:bg-black dark:border-[1px] dark:border-gray-700 p-2 rounded shadow">
           <p>{hoveredCity}</p>
         </div>
       )}
