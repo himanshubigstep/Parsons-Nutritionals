b5150d68772bf3cbd73f6d183dbf5a627df3ef63
yashu <yashu.s@bigsteptech.com>
2024-11-21 18:19:12 +0530

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    async headers() {
+      return [
+        {
+          source: "/(.*)", // Apply headers to all routes
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "default-src 'self'; script-src 'self'; object-src 'none';", // Example CSP policy
+            },
+            {
+              key: "Strict-Transport-Security",
+              value: "max-age=31536000; includeSubDomains; preload", // HSTS policy
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
